57fc5ac9576126f54262c952632b1ef9733d0327
Derek Ruize <derekruiz@usf.edu>
2025-02-02 18:41:21 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -7,3 +7,12 @@ if (purchaseAmount >= 100 ) {
 }; //Prices $100 and over get a 20% discount
 
 console.log('Final amount after discount:', purchaseAmount );
+
+//Task 2
+let sales = [25, 150, 250, 400, 900] //Array of Sales
+let SalesReport = 0 
+
+for (let index = 0; index < sales.length; index++) {
+    console.log(SalesReport += sales[index]);   
+}; // Loop to calculate sales
+
