27bafe646e639bef5375163ed611f4e98ecb9f2e
Derek Ruize <derekruiz@usf.edu>
2025-02-02 18:39:37 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//Task 1
+let purchaseAmount = 250
+if (purchaseAmount >= 100 ) {
+    purchaseAmount *= 0.8
+} else {
+    purchaseAmount
+}; //Prices $100 and over get a 20% discount
+
+console.log('Final amount after discount:', purchaseAmount );
